dac7a374c42f2fda7994c19f74b63ccf1fced94c
Prithwi <prithwi.online11@gmail.com>
2024-12-11 03:57:09 +0530

    Added Other Event APIs

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,6 +1,4 @@
-
 import ensureAuthenticated from "../middlewares/Auth/Auth.js"
-
 
 import express from "express";
 import {
@@ -8,6 +6,12 @@ import {
   createEvent,
   updateEvent,
   deleteEvent,
+  getEventsByStatus,
+  getEventById,
+  getMyEvents,
+  registerForEvent,
+  getEventRequests,
+  handleEventRequest
 } from "../controllers/eventController.js";
 
 const router = express.Router();
@@ -23,5 +27,23 @@ router.put("/update/:id", ensureAuthenticated, updateEvent);
 
 // Route to delete an event by ID
 router.delete("/delete/:id", ensureAuthenticated, deleteEvent);
+
+// Route to get events by ID
+router.get('/getEvent/:id', ensureAuthenticated, getEventById)
+
+// Route to get events by their status
+router.get("/status", ensureAuthenticated, getEventsByStatus);
+
+// Route to get Events created by me
+router.get("/getMyEvents", ensureAuthenticated, getMyEvents);
+
+// Route to Register for an event
+router.post("/:eventId/register", ensureAuthenticated, registerForEvent);
+
+// Route to Get event requests
+router.get("/:eventId/requests", ensureAuthenticated, getEventRequests);
+
+// Route to Approve or reject user request
+router.post("/:eventId/handle-request", ensureAuthenticated, handleEventRequest);
 
 export default router;
