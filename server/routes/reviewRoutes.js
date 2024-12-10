ad1646b0af8c5161c208b0d43be8fa48c90be334
Nirmith Shettigar <nirmith10@gmail.com>
2024-12-10 21:09:34 +0530

    endpoints for review model created

---
+++ server/routes/reviewRoutes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import {
+  createReview,
+  deleteReview,
+  getReviews,
+  updateReview,
+} from "../controllers/reviewController.js";
+import ensureAuthenticated from "../middlewares/Auth.js";
+import { editReviewValidation, newReviewValidation } from "../middlewares/reviewValidation.js";
+
+const router = new Router();
+
+// get request
+router.get("/user/:id", getReviews);
+
+// pot request
+router.post("/new", ensureAuthenticated, newReviewValidation, createReview);
+
+// put request
+router.put("/edit/:reviewId", ensureAuthenticated, editReviewValidation, updateReview);
+
+// delete request
+router.delete("/remove/:reviewId", ensureAuthenticated, deleteReview);
+
+export default router;
