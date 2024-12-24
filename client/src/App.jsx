9abbc0fba9d6c295c8bf964893e6d08631858fe6
Prithwi <prithwi.online11@gmail.com>
2024-12-25 01:51:57 +0530

    Event creation Integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import AppLayout from "./layout/AppLayout";
 import { SignupCard } from "./components/Auth/SignupCard";
 import { LoginCard } from "./components/Auth/LoginCard";
 import ReportUserForm from "./components/Forms/ReportUserForm";
-import EventCreationForm from "../src/components/HelperComponents/EventCreation";
+import EventCreationForm from "./components/Forms/EventCreation";
 import { AuthProvider } from "./context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -25,19 +25,22 @@ function App() {
     <AuthProvider>
       <Routes>
         <Route path="/" element={<AppLayout />}>
-          <Route index element={ <LandingPage /> }/>
+          <Route index element={<LandingPage />} />
 
           {/* Public routes */}
           <Route path="signup" element={<SignupCard />} />
           <Route path="login" element={<LoginCard />} />
           <Route path="events" element={<EventListPage />} />
           <Route path="users" element={<UsersPage />} />
-          <Route path="allSuggestedLearning" element={<AllSuggestedLearningPage/>} />
+          <Route
+            path="allSuggestedLearning"
+            element={<AllSuggestedLearningPage />}
+          />
 
           {/*Protected routes with AppLayout */}
           <Route element={<ProtectedRoute />}>
-          <Route path="/users/:userId" element={<UserProfilePage />} />
-          <Route path="reportuser" element={<ReportUserForm />} />
+            <Route path="/users/:userId" element={<UserProfilePage />} />
+            <Route path="reportuser" element={<ReportUserForm />} />
           </Route>
         </Route>
 
@@ -48,8 +51,7 @@ function App() {
         </Route>
 
         {/* 404 Page for undefined routes */}
-        <Route path="*" element={<NotFoundPage/>} />
-
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <ToastContainer />
     </AuthProvider>
