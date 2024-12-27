b311b881ef3bdf7201827a2a7473c6485806f8b6
Nirmith Shettigar <nirmith10@gmail.com>
2024-12-27 12:57:19 +0530

    review integration done

--- client/src/components/HelperComponents/SessionList.jsx
+++ client/src/components/HelperComponents/SessionList.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { SessionCard } from '../Cards/SessionCard';
 
-export function SessionList({ sessions, onStatusChange, onEdit, onReview, getAllSessions }) {
-  console.log(sessions);
+export function SessionList({ sessions, getAllSessions }) {
   return (
     <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {sessions.map((session) => (
         <SessionCard
           key={session._id}
           session={session}
-          onStatusChange={onStatusChange}
-          onEdit={onEdit}
-          onReview={onReview}
           getAllSessions={getAllSessions}
         />
       ))}
