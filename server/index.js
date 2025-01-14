ae7df5b39ffbccbaa83c879f26b02f318c234e38
Prithwi <prithwi.online11@gmail.com>
2025-01-15 02:00:39 +0530

    cors change

--- server/index.js
+++ server/index.js
@@ -37,16 +37,18 @@ const allowedOrigins = [
   'https://hobby-verse-r94m.vercel.app',
   'https://hobby-verse-ppehfxyra-prithwi-gajanan-hegdes-projects.vercel.app'
 ];
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true,
+// }));
+
+app.use(cors());
 
 // app.use(cors({
 //     origin: function (origin, callback) {
