219d8f3e00df80650961e5bc90f1669144733aa9
Nirmith Shettigar <nirmith10@gmail.com>
2025-01-14 23:05:43 +0530

    require changed import

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: [
@@ -60,5 +62,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
